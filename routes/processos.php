8ce6844267ea3380aa5858f50ea971aef27895e4
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2026-05-16 13:15:17 -0300

    feat: ajustes nas rotas de update|delete do processo;

--- routes/processos.php
+++ routes/processos.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('processos/{cliente}', [ProcessoController::class, 'index'])->name('processos.index'); //Acessar a view de listagem
 
-Route::get('processos/editar/{cliente}', [ProcessoController::class, 'edit'])->name('processos.edit'); //Acessar a view de edição de informações
-Route::patch('processos/editar', [ProcessoController::class, 'update'])->name('processos.update'); //Fazer o update de informações
+Route::get('processos/{cliente}/editar/{processo}', [ProcessoController::class, 'edit'])->name('processos.edit'); //Acessar a view de edição de informações
+Route::patch('processos/editar/{processo}', [ProcessoController::class, 'update'])->name('processos.update'); //Fazer o update de informações
 
 //Incluir
 Route::get('processos/incluir/{cliente}', [ProcessoController::class, 'create'])->name('processos.create'); // Acessar a view
 Route::post('processos/incluir', [ProcessoController::class, 'store'])->name('processos.store'); // Salva as informações
 
 //Deletar
-Route::get('processos/excluir/{processo}', [ProcessoController::class, 'destroy'])->name('processos.destroy'); //Deleta de fato
+Route::get('processos/{cliente}/excluir/{processo}', [ProcessoController::class, 'destroy'])->name('processos.destroy'); //Deleta de fato
 
